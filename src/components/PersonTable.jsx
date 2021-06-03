cfee27aaff255d225a8cb1ae2db9cc7420bfd70f
Haris Gusinac <hgusinac@gmail.com>
2021-06-03 10:55:10 +0200

    Api assigment

---
+++ src/components/PersonTable.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const PersonTable = (props) => {
+    const rows = props.persons.map((person)=>{
+        return (
+            <tr key={person.id}>
+                <td>{person.name}</td>
+                <td>{person.phone}</td>
+                <td>{person.inCityId}</td>
+                
+                <td>
+                <button 
+                onClick={() => {
+                    props.showPerson(person.id);
+                }}
+                className="btn btn-info">
+            Details</button>
+            </td>
+            </tr>
+        );
+      });
+      
+
+      return (
+          <div className="col-md-6 middle-bar">
+              <table className="table table-striped table-bordered">
+                  <thead className="thead-dark">
+                      <tr>
+                          <th>Name</th>
+                          <th>Phone</th>
+                          <th>City</th>
+                          <th>Action</th>
+                          
+                      </tr>
+                  </thead>
+                  <tbody>{rows}</tbody>
+              </table>
+          </div>
+      );
+};
+
+export default PersonTable;
